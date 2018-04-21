2b1f79149e4d0b14b742d0451240bbd6f4862d3a
Michael Saunby <mike@saunby.net>
2018-04-21 22:15:40 +0100

    .

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 # softbatkit-app.py
 from bottle import run, route, get, post, request
+from tuner import tuner_top_block
+
+tb = None
 
 @route('/')
 def home():
@@ -9,5 +12,32 @@ def home():
 def about():
     return "Softbatkit web control app."
 
+@get('/freq')
+def freq():
+    data = {}
+    data['f'] = 1
+    return """
+<form action="/freq" method="post">
+  <input type="text" name="f" />
+  <input type="submit" value="submit" />
+</form>"""
+
+@post('/freq')
+def do_freq():
+    global tb
+    f = request.forms.get('f')
+    data = {}
+    data['f'] = f
+    tb.set_freq( float(f) )
+
+    return data
+
+
+
 if __name__ == '__main__':
+    loop_in = "hw:Loopback,0,0"  # The other side of this device is hw:Loopback,1,0
+    loop_out = "hw:Loopback,1,1" # The other side of this device is hw:Loopback,0,1
+    global tb
+    tb = tuner_top_block( loop_in, 96000, loop_out, 48000, 50000, 1.0)
+    tb.start()
     run(host='localhost', port=8080, debug=True)
--- server/tuner.py
+++ server/tuner.py
@@ -9,7 +9,7 @@ from gnuradio import audio, analog, blocks, filter
 
 class tuner_top_block(gr.top_block):
 
-    def __init__(self, audio_input, input_rate, audio_output, output_rate, frequency=50000, ampl=1.0):
+    def __init__(self, audio_input, input_rate, audio_output, output_rate, frequency=55000, ampl=1.0):
         gr.top_block.__init__(self)
 
         if ampl > 1.0: ampl = 1.0
@@ -21,7 +21,7 @@ class tuner_top_block(gr.top_block):
         src = audio.source (input_rate, audio_input)
         firdes_taps = filter.firdes.low_pass_2(1, 1, 0.2, 0.1, 60)
         self._converter = filter.freq_xlating_fir_filter_ccf ( 1, firdes_taps, 0, input_rate )
-        self._converter.set_center_freq(0 - frequency)
+        self._converter.set_center_freq(frequency)
         dst = audio.sink (output_rate, audio_output, True)
 
         #self.connect(src, converter, to_real, dst)
@@ -31,12 +31,14 @@ class tuner_top_block(gr.top_block):
         self.connect(self._converter, to_real, (dst,0))
         self.connect(self._converter, to_imag, (dst,1))
 
+    def set_freq(self, f):
+        self._converter.set_center_freq(f)
 
 if __name__ == '__main__':
     try:
         loop_in = "hw:Loopback,0,0"  # The other side of this device is hw:Loopback,1,0
         loop_out = "hw:Loopback,1,1" # The other side of this device is hw:Loopback,0,1
-        tb = tuner_top_block( loop_in, 96000, loop_out, 48000, 50000, 1.0)
+        tb = tuner_top_block( loop_in, 96000, loop_out, 48000, 55000, 1.0)
         tb.start()
         resp = raw_input("quit now? ")
         tb.stop()
