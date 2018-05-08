8a60d088d87b933c5829a13d858c2b35fb4d1230
Michael Saunby <mike@saunby.net>
2018-05-08 09:43:19 +0100

    .

--- pi-streaming/stream-line-mic.sh
+++ pi-streaming/stream-line-mic.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 Record_from_Linein_Micbias.sh
-arecord -f S16_LE -c2 -r44000 | ffmpeg -i -  -b:a 512k -bufsize 2M -ar 44000 -f mulaw -f rtp rtp://192.168.0.22:1234
+arecord -f S16_LE -c2 -r192000 | ffmpeg -i -  -b:a 512k -bufsize 2M -ar 192000 -f mulaw -f rtp rtp://192.168.0.22:1234
 
 #SDP:
 #v=0
@@ -12,6 +12,6 @@ arecord -f S16_LE -c2 -r44000 | ffmpeg -i -  -b:a 512k -bufsize 2M -ar 44000 -f 
 #a=tool:libavformat 57.56.101
 #m=audio 1234 RTP/AVP 97
 #b=AS:128
-#a=rtpmap:97 PCMU/8000/2
+#a=rtpmap:97 PCMU/192000/2
 
 # SDL_AUDIODRIVER="alsa" AUDIODEV="hw:Loopback,1,0" ffplay  -vn -nodisp -b:a 512k -bufsize 2M -i stream.spd
